96d4047a4f17bd47857ce13af67821b4e1ab1c34
Andrey Dementev <andreasdv1@yandex.ru>
2025-05-14 15:35:42 +0300

    Commit2

--- src/test/java/TestAnimalParametrized.java
+++ src/test/java/TestAnimalParametrized.java
@@ -1,69 +1,36 @@
-import com.example.Cat;
-import com.example.Feline;
-import com.example.Lion;
-import com.example.Predator;
+import com.example.Animal;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.mockito.Mockito;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
+// В данном классе тестируется метод getFood класса Animal
+//  с помощью параметризованных тестов
+// В классе AnimalTest находятся не параметризованные тесты для класса Animal
 @RunWith(Parameterized.class)
-public class ParameterizedTest {
+public class TestAnimalParametrized {
 
-    private final String inputGender;
-    private final boolean expectedHasMane;
-    private final int kittensCount;
+    private final String animalKind;
+    private final List<String> animalFood;
 
-    public ParameterizedTest(String inputGender, boolean expectedHasMane, int kittensCount) {
-        this.inputGender = inputGender;
-        this.expectedHasMane = expectedHasMane;
-        this.kittensCount = kittensCount;
+    public TestAnimalParametrized(String animalKind, List<String> animalFood){
+        this.animalKind = animalKind;
+        this.animalFood = animalFood;
     }
 
     @Parameterized.Parameters
-    public static Collection<Object[]> getParameters() {
-        return Arrays.asList(new Object[][]{
-                {"Самец", true, 3},
-                {"Самка", false, 2}
-        });
+    public static Object[][] animalClassTestData() {
+        return new Object[][]{
+                {"Травоядное", List.of("Трава", "Различные растения")},
+                {"Хищник", List.of("Животные", "Птицы", "Рыба")}
+        };
     }
 
     @Test
-    public void testLionDoesHaveManeAndGetKittens() throws Exception {
-        Feline mockFeline = Mockito.mock(Feline.class);
-        Mockito.when(mockFeline.getKittens()).thenReturn(kittensCount);
-
-        Lion lion = new Lion(inputGender, mockFeline);
-
-        assertEquals(expectedHasMane, lion.doesHaveMane());
-        assertEquals(kittensCount, lion.getKittens());
-    }
-
-    @Test
-    public void testCatGetFood() throws Exception {
-        Predator mockPredator = Mockito.mock(Predator.class);
-        List<String> expectedFood = Arrays.asList("Мясо", "Птицы");
-        Mockito.when(mockPredator.eatMeat()).thenReturn(expectedFood);
-
-        Cat cat = new Cat(mockPredator);
-        assertEquals(expectedFood, cat.getFood());
-    }
-
-    @Test
-    public void testFelineGetKittens() {
-        Feline feline = new Feline();
-        assertEquals(kittensCount, feline.getKittens(kittensCount));
-    }
-
-    @Test
-    public void testFelineFamily() {
-        Feline feline = new Feline();
-        assertEquals("Кошачьи", feline.getFamily());
+    public void testGetFood() throws  Exception{
+        Animal animal = new Animal();
+        assertEquals(animalFood, animal.getFood(animalKind));
     }
 }
